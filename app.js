7cfbcccebacb0175ac66af4c4e1ba2532e8258fe
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2021-04-11 02:55:21 +0530

    wip: taking input and console

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// console.log("script is working form a differnet file")
+// // console.log("script is working form a differnet file")
 
-//input
-var username = prompt("give me your user name")
+// //input
+// var username = prompt("give me your user name")
 
-//processing
-var welcomeMessage = " this script works " + username;
+// //processing
+// var welcomeMessage = " this script works " + username;
 
-//output
-alert(welcomeMessage)
+// //output
+// alert(welcomeMessage)
+
+
+
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+console.log(txtInput);
+
+function clickHandler() {
+    console.log("clicked")
+    console.log("input", txtInput.value);
+}
+
+btnTranslate.addEventListener("click",clickHandler)
 
