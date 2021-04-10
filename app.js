664289c1860a8fc7aa1acb9809caaf383f632e3e
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2021-04-11 02:12:34 +0530

    wip: importjavascript

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("script is working form a differnet file")
+
+//input
+var username = prompt("give me your user name")
+
+//processing
+var welcomeMessage = " this script works " + username;
+
+//output
+alert(welcomeMessage)
+
